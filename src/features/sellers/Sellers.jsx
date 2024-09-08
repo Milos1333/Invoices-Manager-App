f961194a683d380f8f26779a9ebeffe5ff28798a
Milos1333 <milosklepic91@gmail.com>
2024-09-09 01:58:54 +0200

    style of create modal

--- src/features/sellers/Sellers.jsx
+++ src/features/sellers/Sellers.jsx
@@ -2,8 +2,11 @@ import { Table } from "antd";
 import "antd/dist/reset.css";
 import "./sellers.style.css";
 import ActionIcons from "../../components/actionIcons/ActionIcons";
+import { useState } from "react";
 
 const Sellers = ({ sellers }) => {
+  const [selectedRowId, setSelectedRowId] = useState(null);
+
   const columns = [
     {
       title: "Company Name",
@@ -27,21 +30,37 @@ const Sellers = ({ sellers }) => {
     },
   ];
 
+  const handleRowClick = (record) => {
+    setSelectedRowId((prevSelectedRowId) =>
+      prevSelectedRowId === record.id ? null : record.id
+    );
+  };
+
   return (
     <div className="invoices-wrapper">
       <div className="header-container">
-        <h2>INVOICES</h2>
+        <h2>SELLERS</h2>
       </div>
       <div className="action-icons-container">
-        <ActionIcons />
+        <ActionIcons
+          selectedRowId={selectedRowId}
+          data={sellers}
+          type="seller"
+        />
       </div>
       <div className="invoices-table-container">
         {sellers.length > 0 ? (
           <Table
             dataSource={sellers}
             columns={columns}
-            rowKey="id" // Jedinstveni identifikator za svaki red
-            pagination={{ pageSize: 5 }} // Broj redova po stranici
+            rowKey="id"
+            pagination={{ pageSize: 5 }}
+            onRow={(record) => ({
+              onClick: () => handleRowClick(record),
+            })}
+            rowClassName={(record) =>
+              record.id === selectedRowId ? "selected-row" : ""
+            }
           />
         ) : (
           <p>No sellers available</p>
